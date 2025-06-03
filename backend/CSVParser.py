52642d7d15f1bbd736524bcd693b02dbb33ce9de
Adam Kerns <157764606+AdamKerns@users.noreply.github.com>
2025-06-03 03:33:37 -0400

    200MoviesWatched.csv finished and able to be parsed and put into DB

---
+++ backend/CSVParser.py
@@ -0,0 +1,54 @@
+import csv
+
+# If file (200MoviesWatched.csv) exists do this
+'''
+    used to test the database by initializing movies
+    will progress to updating functionality / inputing movies directly throught the software
+'''
+def initialMovies():
+    
+    with open("frontend/static/csv-files/200MoviesWatched.csv", 'r') as CSVfile:
+        movieReader = csv.DictReader(CSVfile)
+        i = 0
+        for row in movieReader:
+            title = row["Title"].strip()
+            title = title.replace("`", ",") # USE
+            pScore = row["Prime Score"].strip() # rated out of 5    USE
+            rtScore = row["Rotten Tomatoes Score"].strip() # rated out of 10    USE
+            iScore= row["IMDb Score"].strip() # rated out of 10     USE
+            averageScore = ((2 * float(pScore)) + float(rtScore) + float(iScore)) / 3   # USE
+            """ Testing
+            i += 1
+            print("~~~~~~~~~~~~~~~~~~")
+            print(f"{i}. {title}")
+            print("---------")
+            print(f"Prime Score: {pScore}/5     Rotten Tomatoes Score: {rtScore}/10     IMDb Score: {iScore}/10")
+            print(f"Average Score: {averageScore:.1f}/10")
+            print("---------")
+            print("~~~~~~~~~~~~~~~~~~")
+            """
+            link = row["OnionPlay Link"].strip()    # USE
+            genres = row["Genre List"].strip()  
+            genreList = genres.split(" ")   # USE
+            # print(genreList)  Testing
+            myScore = row["My Score"].strip()   # USE
+            iSummary = row["IMDb Summary"].strip()  # USE
+            awards = row["Awards"].strip()
+            awardAmount = 0     # USE
+            awardsList = awards.split(" + ")    # USE
+            # print(awardsList) Testing
+            for award in awardsList:
+                amount = 0 if award == '' else 1
+                if "s" == award[-1:] or (award.find("Awards") != -1 and award.find("Academy") != -1):
+                    # print(award) Testing
+                    amount = float(award[0])
+                awardAmount += amount    
+            # print(awardAmount)    Testing
+            mustWatch = row["Must Watch"].strip()
+            mustWatch = True if mustWatch == "True" else False  # USE 
+            
+            # put into database
+             
+           
+            
+initialMovies()
