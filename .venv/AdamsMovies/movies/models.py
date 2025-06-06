687656ca8bb57487156999632d29355f8912f4ee
Adam Kerns <157764606+AdamKerns@users.noreply.github.com>
2025-06-06 13:52:51 -0400

    created Movie model and added some movies

--- .venv/AdamsMovies/movies/models.py
+++ .venv/AdamsMovies/movies/models.py
@@ -6,3 +6,8 @@ class Movie(models.Model):
     rtScore = models.FloatField()
     iScore = models.FloatField()
     link = models.CharField(max_length=255)
+    genre_list = models.CharField(max_length=255)
+    myScore = models.FloatField(max_length=255)
+    summary = models.CharField(max_length=510)
+    award_list = models.CharField(max_length=510)
+    must_watch = models.BooleanField()
